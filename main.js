69d11bb8df7b8bdcf91eb4bd74b68bd477d981fc
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-02-14 11:16:43 +0530

    done

--- main.js
+++ main.js
@@ -19,12 +19,12 @@ const btn = document.querySelector('.btn');
 const randomColorBlock = document.querySelectorAll('.box');
 
 function RandomHexColorCode () {
-    var chars = '0123456789abcdef';
-    var colorLength = 6;
-    var color = '';
+    let chars = '0123456789abcdef';
+    let colorLength = 6;
+    let color = '';
 
-    for (var i = 0; i < colorLength; i++) {
-        var randomColor = Math.floor(Math.random() * chars.length);
+    for (let i = 0; i < colorLength; i++) {
+        let randomColor = Math.floor(Math.random() * chars.length);
         color += chars.substring(randomColor, randomColor + 1);
     }
     return '#' + color;
@@ -32,7 +32,7 @@ function RandomHexColorCode () {
 
 function addColor () {
     randomColorBlock.forEach(e => {
-        var newColor = RandomHexColorCode();
+        let newColor = RandomHexColorCode();
         e.style.backgroundColor = newColor;
         e.innerHTML = newColor;
     });
